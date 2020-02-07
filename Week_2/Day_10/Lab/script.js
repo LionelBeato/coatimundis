9bd652b90db71b2e6d57784a2d73de6a4d8257ac
LionelBeato <lionel.beato@gmail.com>
2020-02-07 14:38:34 -0500

    adds homework example, feel free to copy/paste it and play around with it!

--- Week_2/Day_10/Lab/script.js
+++ Week_2/Day_10/Lab/script.js
@@ -1,21 +1,28 @@
 
-function myFunc() {
-    try{
-        //the throw statement allows you to create a custom error
-        throw "hey stop that!"
+// function myFunc() {
+//     try{
+//         //the throw statement allows you to create a custom error
+//         throw "hey stop that!"
         
-    } catch (error){
-        //this error will refer back to my custom error in my try block 
-        console.log(error)
-    }
-}
+//     } catch (error){
+//         //this error will refer back to my custom error in my try block 
+//         console.log(error)
+//     }
+// }
 
-try{
-    let x = 3
-    throw x
-} catch (e){
-    console.log(e)
-}
+// try{
+//     let x = 3
+//     throw x
+// } catch (e){
+//     console.log(e)
+// }
+
+
+let x = 5
+let y = "4"
+let z = x+parseInt(y)
+
+console.log(z)
 
 
 
