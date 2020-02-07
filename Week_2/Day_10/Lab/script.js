94afbbf93dd5001a920883d3663beee85730acbf
LionelBeato <lionel.beato@gmail.com>
2020-02-07 10:53:09 -0500

    adds lab and assignment

--- Week_2/Day_10/Lab/script.js
+++ Week_2/Day_10/Lab/script.js
@@ -1,9 +1,22 @@
 
+function myFunc() {
+    try{
+        //the throw statement allows you to create a custom error
+        throw "hey stop that!"
+        
+    } catch (error){
+        //this error will refer back to my custom error in my try block 
+        console.log(error)
+    }
+}
 
 try{
-    myFunc()
-
-} catch (error){
-    console.log(error)
+    let x = 3
+    throw x
+} catch (e){
+    console.log(e)
 }
+
+
+
 
