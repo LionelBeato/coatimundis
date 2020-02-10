5775c9752fab4b8b68b847667a96af6322e3b51c
LionelBeato <lionel.beato@gmail.com>
2020-02-10 16:40:23 -0500

    finalizes backend/frontend example to a working state

--- Week_3/Day_11/backend/server.js
+++ Week_3/Day_11/backend/server.js
@@ -11,17 +11,20 @@
 
 const express = require('express')
 const app = express()
+
+//port declaration to allow for local development or heroku 
 const port = process.env.PORT || 3000
 
 let apples = {name:'apples', amount:4}
 
 app.use(function(req, res, next) {
+    // header values to get around CORS
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 
-
+// get http functions 
 app.get('/', (req, res) => res.send('hello there!'))
 // app.get('/index', (req, res) => res.sendFile('index.html', {root:__dirname}))
 app.get('/data', (req, res) => res.json(apples))
--- Week_3/Day_12/OVERVIEW.md
+++ Week_3/Day_12/OVERVIEW.md
@@ -1,4 +1,4 @@
-# February 10, 2019 - Day 11
+# February 11, 2019 - Day 12 
 
 This is the OVERVIEW.md or today's class. This file serves as a rough outline of our day. 
 
