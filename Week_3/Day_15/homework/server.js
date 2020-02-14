4322458105b5cc3b07e7c33e214751b859ceaa0d
LionelBeato <lionel.beato@gmail.com>
2020-02-14 11:26:17 -0500

    adds working mongodb code

--- Week_3/Day_15/homework/server.js
+++ Week_3/Day_15/homework/server.js
@@ -3,8 +3,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose')
+const uri = "mongodb+srv://Admin:mypassword@cluster0-uu8vk.mongodb.net/test?retryWrites=true&w=majority"
 
 
 //express boilerplate 
 app.get('/',(req, res) => res.send("Hello World!"))
 app.listen(port, () => console.log(`Listening on port ${port}`))
+
+//mongoose code
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology:true})
+
+const Dog = mongoose.model("Dog", {name:String})
+const rover = new Dog ({name:'Rover'})
+const rex = new Dog ({name:'Rex'})
+
+
+
+rover.save().then(() => console.log('dog was saved'))
+Dog.find().then(res => console.log(res))
