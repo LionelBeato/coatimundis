a6cf0f27cb367dfef5da5e07c2a80fbf38a3dbb4
LionelBeato <lionel.beato@gmail.com>
2020-02-18 11:11:45 -0500

    adds typescript

---
+++ Week-4/Day-17/typescript-basics/mod.ts
@@ -0,0 +1,23 @@
+// here is a list of all the primitives types in javascript
+// Boolean
+// Number
+// String
+// Symbol (represents a unique, immutable identifier, sometimes called an atom)
+// Null (0, empty)
+// Undefined (the stock primitive value of variables)
+// BigInt
+
+// The other type for javascript is:
+// Object 
+// in javascript, functions are also objects
+
+
+let user:string = "bob"
+let num:number = 33
+
+// there are typecasting on certain objects in javascript/typescript
+// here, a number is being casted to a string 
+let numString = num.toString()
+
+// typeof lets you check the type of any given object
+console.log(typeof num)
