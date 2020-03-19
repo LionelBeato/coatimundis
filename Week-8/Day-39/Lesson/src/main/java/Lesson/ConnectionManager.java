8290bb76b10ca83a35a04005e9d9ac84c5cc7d49
Lionel Beato <leo@localhost.localdomain>
2020-03-19 13:24:18 -0400

    adds completed lesson

--- Week-8/Day-39/Lesson/src/main/java/Lesson/ConnectionManager.java
+++ Week-8/Day-39/Lesson/src/main/java/Lesson/ConnectionManager.java
@@ -5,21 +5,31 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+// in Java, there's concept called JDBC
+// it stands for Java DataBase Connectivity
+// JDBC allows you to connect your Java code to a SQL/noSQL database
+// we connect via "drivers" that give us accessibility
+
 
 // this class is helps me construct a new connection to a database
 // using jdbc configurations in code
 public class ConnectionManager {
 
+    // this is the url of our connection
     private final String url;
+    // this field defines our properties
     private final Properties properties;
 
     public ConnectionManager(String host, String databaseName, String username, String password) {
+        // this is defining our url connection
         this.url = "jdbc:mariadb://"+host+"/"+databaseName;
         this.properties = new Properties();
+        // this is assigning the username and password
         this.properties.setProperty("user", username);
         this.properties.setProperty("password", password);
     }
 
+    // this method allows us to actually commit the connection
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(this.url, this.properties);
     }
--- Week-8/Day-39/Lesson/src/main/java/Lesson/Lesson.java
+++ Week-8/Day-39/Lesson/src/main/java/Lesson/Lesson.java
@@ -12,16 +12,22 @@ public class Lesson {
         // constructing an instance of my connection manager
         // note the values being passed in
         ConnectionManager dcm = new ConnectionManager("localhost", "test", "root", "");
+
         // simple try and catch block to handle errors
         // note the use of of resources with our try block
         try (Connection connection = dcm.getConnection()){
+
             // this allows use to write a statement with our connection
             Statement statement = connection.createStatement();
+
             // manually declaring a query
             ResultSet resultSet = statement.executeQuery("SELECT * FROM songs");
+
             // printing the result of our query
             // resultSet is like a cursor that will point to a certain position in your database
+            // this while loop will stop once there isn't a next value to read
             while (resultSet.next()) {
+
                 // this print statement is pointing to the second column "song_title"
                 // it'll return the values accordingly
                 System.out.println(resultSet.getString(2));
@@ -29,6 +35,14 @@ public class Lesson {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+
+        // You may be wondering how JPA is related to JDBC
+        // JPA stands for Java Persistance Abstraction
+        // It's more or less an abstraction of JDBC
+        // In Spring Boot we use JPA
+        // JPA is an abstraction layer on top of JDBC
+        // it allows us the ability to not have to write SQL code
 
     }
 
