71f21f1d7e022e1021970989dccb23777c39d893
Lionel Beato <leo@localhost.localdomain>
2020-04-06 09:02:34 -0400

    adds bulma example

---
+++ Week-11/Day-51/bulma.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+// import bulma.css here from node_modules
+
+const bulmaPage = () => {
+  // javascript code
+  return (
+    <main>
+      <section>
+        <div className="container is-fluid">
+          <div className="columns">
+            <div className="column">
+                {/* Content */}
+            </div>
+            <div className="column">
+                {/* Content */}
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
