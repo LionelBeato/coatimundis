be592ba97b9ea3eb79e31df0e0eb9752ef8c7784
Lionel Beato <leo@localhost.localdomain>
2020-04-06 09:28:13 -0400

    adds working gatsby bulma example

---
+++ Week-11/Day-51/sample-bulma/src/pages/bulma.js
@@ -0,0 +1,27 @@
+import React from "react"
+import "bulma/css/bulma.css"
+
+const BulmaPage = () => {
+    return (
+        <main>
+            <section className="section is-fluid">
+                <div className="container is-fluid">
+                    <div className="columns">
+                        <div className="column">
+                            <p>This is inside of a column!</p>
+                        </div>
+                        <div className="column">
+                            <p>This is inside of a column!</p>
+                        </div>
+                        <div className="column">
+                            <p>This is inside of a column!</p>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+        </main>
+    )
+}
+
+export default BulmaPage
