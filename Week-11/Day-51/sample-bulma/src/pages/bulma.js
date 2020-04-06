dce3e905a24f0eb7691e4f9f6730d6c62ba2fc26
Lionel Beato <leo@localhost.localdomain>
2020-04-06 09:30:48 -0400

    adds cleans up code

--- Week-11/Day-51/sample-bulma/src/pages/bulma.js
+++ Week-11/Day-51/sample-bulma/src/pages/bulma.js
@@ -2,26 +2,25 @@ import React from "react"
 import "bulma/css/bulma.css"
 
 const BulmaPage = () => {
-    return (
-        <main>
-            <section className="section is-fluid">
-                <div className="container is-fluid">
-                    <div className="columns">
-                        <div className="column">
-                            <p>This is inside of a column!</p>
-                        </div>
-                        <div className="column">
-                            <p>This is inside of a column!</p>
-                        </div>
-                        <div className="column">
-                            <p>This is inside of a column!</p>
-                        </div>
-                    </div>
-                </div>
-
-            </section>
-        </main>
-    )
+  return (
+    <main>
+      <section className="section is-fluid">
+        <div className="container is-fluid">
+          <div className="columns">
+            <div className="column">
+              <p>This is inside of a column!</p>
+            </div>
+            <div className="column">
+              <p>This is inside of a column!</p>
+            </div>
+            <div className="column">
+              <p>This is inside of a column!</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  )
 }
 
 export default BulmaPage
